b2eb386b4884e2e90844c199d47f478205f97eac
Jaehyun An <lvet0330@gmail.com>
2022-09-19 17:33:14 +0900

    220919

--- clac-prac/src/test/java/org/example/CalculatorTest.java
+++ clac-prac/src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CalculatorTest {
@@ -16,7 +19,7 @@ public class CalculatorTest {
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void calculateTest(int operand1, String operator, int operand2, int result){
-        int calculatorResult = Calculator.calculate(operand1,operator,operand2);
+        int calculatorResult = Calculator.calculate(new PositiveNumber(operand1),operator,new PositiveNumber(operand2));
 //        Assertions.assertEquals(result,3);
         assertThat(calculatorResult).isEqualTo(result);
     }
@@ -27,5 +30,13 @@ public class CalculatorTest {
                 arguments(4, "*", 2, 8),
                 arguments(4, "/", 2 ,2)
         );
+    }
+    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgumet 예외를 발생시킨다.")
+    @Test
+    //PositiveNumber 때문에 의미없는 코드가 됐음
+    void calculateExceptionTest(){
+        assertThatCode(()->Calculator.calculate(new PositiveNumber(10),"/", new PositiveNumber(0)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0또는 음수를 전달할 수 없습니다.");
     }
 }
