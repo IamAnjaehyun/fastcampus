e3c9ea064466f7910eef849ab65a1c7773b4ea9e
Jaehyun An <lvet0330@gmail.com>
2022-09-25 19:39:33 +0900

    220925

--- foodcustomer-prac/src/main/java/oop/practice/Cook.java
+++ foodcustomer-prac/src/main/java/oop/practice/Cook.java
@@ -1,8 +1,28 @@
 package oop.practice;
 
+import java.util.Objects;
+
 public class Cook {
     private final String name;
     private final int price;
+
+    public Cook(MenuItem menuItem) {
+        this.name = menuItem.getName();
+        this.price = menuItem.getPrice();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cook cook = (Cook) o;
+        return price == cook.price && Objects.equals(name, cook.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
 
     public Cook(String name, int price) {
         this.name = name;
