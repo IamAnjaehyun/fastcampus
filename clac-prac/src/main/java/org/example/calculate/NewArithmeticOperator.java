af8348b2cd8c6a9c1e91bd4f72749ff02c872ef3
Jaehyun An <lvet0330@gmail.com>
2022-09-25 22:07:47 +0900

    220925

--- clac-prac/src/main/java/org/example/calculate/NewArithmeticOperator.java
+++
@@ -1,6 +0,0 @@
-package org.example.calculate;
-
-public interface NewArithmeticOperator {
-    public boolean supports(String operator);
-    int calculate(PositiveNumber operand1, PositiveNumber operand2);
-}
