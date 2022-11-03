abbe4523dad5d7fbaf6786e91fedb117b54c8755
Jaehyun An <lvet0330@gmail.com>
2022-11-03 23:26:06 +0900

    # 객체연습
    easy

---
+++ fastcampusPart2/std_java/untitled/src/collab/Bus.java
@@ -0,0 +1,20 @@
+package collab;
+
+public class Bus {
+    int busNumber;
+    int passengerCount;
+    int money;
+
+    public Bus(int busNumber){
+        this.busNumber = busNumber;
+    }
+
+    public void take(int money){
+        this.money += money;
+        passengerCount++;
+    }
+
+    public void showBusinfo(){
+        System.out.println(busNumber + "번의 승객 수는 "+passengerCount+"이며 번 금액은 "+money+"입니다.");
+    }
+}
