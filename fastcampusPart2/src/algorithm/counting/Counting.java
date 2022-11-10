dd2cf61d6513ddc57f8cbb948c662986afe8048d
Jaehyun An <lvet0330@gmail.com>
2022-11-10 21:19:45 +0900

    # 나이분류
    여러명의 사람들의 나이를 분류하기

---
+++ fastcampusPart2/src/algorithm/counting/Counting.java
@@ -0,0 +1,31 @@
+package algorithm.counting;
+
+public class Counting {
+    //여러명의 사람들의 나이를 분류하기
+    public static void main(String[] args) {
+        int[] people = {55,40,27,99,76,81,29,31,33,62};
+        int[] ages = new int[10]; // 연령대에 따라 각각 지정
+
+        for(int i=0; i<people.length;i++){
+            int age = people[i];
+            if(age<30) ages[0]++;
+            else if (age < 40) ages[1]++;
+            else if (age < 50) ages[2]++;
+            else if (age < 60) ages[3]++;
+            else if (age < 70) ages[4]++;
+            else if (age < 80) ages[5]++;
+            else if (age < 90) ages[6]++;
+            else if (age <= 100) ages[7]++;
+        }
+        int number = people.length;
+
+        System.out.println(number + "명 중에 20대는 "+ages[0]+"명 입니다.");
+        System.out.println(number + "명 중에 30대는 "+ages[1]+"명 입니다.");
+        System.out.println(number + "명 중에 40대는 "+ages[2]+"명 입니다.");
+        System.out.println(number + "명 중에 50대는 "+ages[3]+"명 입니다.");
+        System.out.println(number + "명 중에 60대는 "+ages[4]+"명 입니다.");
+        System.out.println(number + "명 중에 70대는 "+ages[5]+"명 입니다.");
+        System.out.println(number + "명 중에 80대는 "+ages[6]+"명 입니다.");
+        System.out.println(number + "명 중에 90대는 "+ages[7]+"명 입니다.");
+    }
+}
