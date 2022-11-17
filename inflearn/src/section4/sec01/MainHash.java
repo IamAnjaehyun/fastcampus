9cbeaa2cc380416889e1d25d2b5e6f4a2c4822e6
Anjaehyun <lvet0330@gmail.com>
2022-11-17 23:31:28 +0900

    # sec01 - HashMap 공부

---
+++ inflearn/src/section4/sec01/MainHash.java
@@ -0,0 +1,47 @@
+package section4.sec01;
+//학급 회장 뽑기 HashMap(key:value) key데이터형 char(abcd) / value데이터형 int(카운팅 해야됨)
+//가장 표 많이 받은 사람 당선
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MainHash {
+    public static char Solution(int n, String s) {
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s.toCharArray()) { //x가 a가됐다 b가됐다 c가됐다 ...
+            map.put(x, map.getOrDefault(x, 0) + 1); //getOrDefault(x가 있다면 x의 키값, x의 키값이 없으면 0을 가져옴) +1은 기본 키값에 1씩 더하는겨
+        }//키를 알파벳 갯수만큼 계산하는 for 문
+
+
+        System.out.println("------------");
+        System.out.println("contatinsKey "+map.containsKey('A')); // '~' 라는 키가 있는지 확인하는거 있으면 true를 리턴함 (boolean형)
+        System.out.println(map.containsKey('F')); // '~' 라는 키가 있는지 확인하는거 없으면 false를 리턴함(boolean형)
+        System.out.println(map.size()); //키의 종류 개수를 알려줌
+        System.out.println("------------");
+        System.out.println("remove가 A의 갯수를 출력함"+map.remove('A')); //특정 키를 삭제함
+        System.out.println("삭제하고 난 뒤의 키 개수+"+ map.size()); //키의 종류 개수를 알려줌
+        System.out.println("------------");
+
+
+        int max = Integer.MIN_VALUE;
+        for (char key : map.keySet()) {
+//            System.out.println(x+" : "+map.get(x)); // key = x / map.get(x) = x의 value
+            if (map.get(key) > max) {
+                max = map.get(key);
+                answer = key;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        System.out.println("문제 정답 " + Solution(n, str));
+    }
+}
+
